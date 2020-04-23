9d132ae4c0b6f82a5b220cc98a878b713220fa9f
AlexZhaba <programzhava@mail.ru>
2020-04-24 01:41:56 +0300

    sec;

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var app = express();
+let {Pool, Client } = require('pg');
+
+const pool = new Pool({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'vk_app',
+	password: 'postgres',
+	port: 5432,
+	// ssl: true
+});
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	pool.connect((err, client, done) => {
+		if (err) throw err;
+		client.query(`SELECT * FROM USERS`, (err, result) => {
+			if (err) throw;
+			done();
+			res.json(result.rows);
+		})
+	});
+//   res.send('Hello World!');
 });
 
 app.listen(3000, function () {
