6bad077617395936974c264688c5b9380172433c
AlexZhaba <programzhava@mail.ru>
2020-04-30 10:50:43 +0300

    2

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ let setResponseHeaders = (response) => {
 	//https://alexzhaba.github.io
 	//194.58.107.149:3000
 	//http://localhost:10888
-	// response.setHeader("Access-Control-Allow-Origin", "https://alexzhaba.github.io");
-	response.setHeader("Access-Control-Allow-Origin", "http://localhost:10888");
+	response.setHeader("Access-Control-Allow-Origin", "https://alexzhaba.github.io");
+	// response.setHeader("Access-Control-Allow-Origin", "http://localhost:10888");
 	// response.setHeader("Access-Control-Allow-Origin", "194.58.107.149:3000");
 
 }
@@ -44,9 +44,10 @@ app.get('/getUserById/:id', (req, res) => {
 app.post('/addQuestToUser', (req, res) => {
 	setResponseHeaders(res);
 	let quest = req.body;
+	console.log('quest = ',quest)
 	pool.connect((err, client, done) => {
 		if (err) throw err;
-		client.query(`INSERT INTO QUESTS VALUES(${quest.user_id}, '${quest.text}', '${quest.description}', false, 0, ${Date.now() + 60 * 60 * 24}, 0, ${quest.manaBonus}, ${quest.expBonus}, ${quest.manaDowner}, ${quest.expDowner})`, (err, result) => {
+		client.query(`INSERT INTO QUESTS VALUES(${quest.user_id}, '${quest.text}', '${quest.description}', false, 0, ${Date.now() + 1000 * 60 * 60 * 24}, 0, ${quest.manaBonus}, ${quest.expBonus}, ${quest.manaDowner}, ${quest.expDowner})`, (err, result) => {
 			if (err) throw err;
 			done();
 			res.json({msg: 'added Quest'});
@@ -54,11 +55,22 @@ app.post('/addQuestToUser', (req, res) => {
 	});
 	
 });
+app.post('/setUser', (req, res) => {
+	setResponseHeaders(res);
+	pool.connect((err, client, done) => {
+		if (err) throw err;
+		client.query(`UPDATE USERS SET unusedquests=${req.body.newUnusedQuests} WHERE ID=${req.body.user_id}`, (err, result) => {
+			if (err) throw err;
+			done();
+			res.json({msg: "update"})
+		})
+	})
+});
 app.get('/getQuestsById/:id', (req, res) => {
 	setResponseHeaders(res);
 	pool.connect((err, client, done) => {
 		if (err) throw err;
-		client.query(`SELECT * FROM QUESTS WHERE ID=${req.params.id}`, (err, result) => {
+		client.query(`SELECT * FROM QUESTS WHERE ID=${req.params.id} ORDER BY COMPLETED`, (err, result) => {
 			if (err) throw err;
 			done();
 			res.json({quests: result.rows});
@@ -82,6 +94,27 @@ app.post('/createNewUser', (req, res) => {
 				res.json(result.rows[0]);
 			})
 		});
+	})
+});
+
+app.post('/setQuest', (req, res) => {
+	setResponseHeaders(res);
+	console.log(req.body)
+	let user = req.body.user;
+	pool.connect((err, client, done) => {
+		if (err) throw err;
+		client.query(`UPDATE QUESTS SET completed=${req.body.completed} WHERE TEXT='${req.body.text}'`, (err, result) => {
+			if (err) throw err;
+			client.query(`UPDATE USERS SET mana=${Math.min(100, user.mana + req.body.manaBonus)}, currentlvl=${(user.currentlvl + req.body.expBonus) % user.maxlvlpoints}, `
+			+ `level=${user.currentlvl + req.body.expBonus >= user.maxlvlpoints ? user.level + 1 : user.level}, unusedquests=${user.currentlvl + req.body.expBonus >= user.maxlvlpoints ? user.unusedquests + 2 : user.unusedquests} WHERE ID=${user.id}`, (err, result) => {
+				if (err) throw err;
+				console.log('result = ', result)
+				done();
+				let newLvl = user.currentlvl + req.body.expBonus >= user.maxlvlpoints ? user.level + 1 : 0;
+				
+				res.json({msg: 'UPDATE', newLvl: newLvl, unusedquests: newLvl ? user.unusedquests + 2 : user.unusedquests})
+			});
+		})
 	})
 });
 app.listen(3000, function () {
